e014e182a0ef4099c89995e6276c40253a006908
Jose Manuel <56323216+s2josemanuels2@users.noreply.github.com>
2020-04-21 00:00:27 -0500

    Corrección de webcam

--- templates/js/script.js
+++ templates/js/script.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
 	ctx.height = canvas.height;
 	var usuario = prompt("Ingresa tu usuario");
 	var video = document.getElementById("video");
-
+	$('#titulo').text(usuario);
 	var socket = io.connect('https://envivoapp.herokuapp.com:/',{ 'forceNew': true });
 	var socket2 = io.connect('https://envivoapp.herokuapp.com/',{ 'forceNew': true });
 
 	socket2.on('streaming',function(data){
-		if(data['user'] !== usuario){
+		if(data['user'] && data['user'] !== usuario){
 			if ($('#'+data['user']).length === 0) {
+				$('body').append('<h2>'+data['user']+'</h2>');
 				$('body').append('<img id="'+data['user']+'" width="400px" height="400px">');
 			}
 			
@@ -40,17 +41,17 @@ $(document).ready(function(){
 		socket.emit('stream',{'img':canvas.toDataURL('image/webp'), 'user':usuario});
 	}
 
-	navigator.getUserMedia = (navigator.getUserMedia ||
-	 navigator.webkitGetUserMedia || 
-	 navigator.mozGetUserMedia || 
-	 navigator.msgGetUserMedia);
-
+	navigator.getUserMedia = ( 
+		navigator.getUserMedia ||
+		navigator.webkitGetUserMedia || 
+		navigator.mozGetUserMedia || 
+		navigator.msGetUserMedia);
 	if (navigator.getUserMedia) {
 		//atributos de uso, callbacks
+		//navigator.mediaDevices.getUserMedia({video:true},loadCam,loadFail);
 		navigator.getUserMedia({video:true},loadCam,loadFail);
 		setInterval(function(){
 			viewVideo(video,ctx);
 		}, 70);
 	}
-	//var socket2 = io.connect('http://127.0.0.1:8000');
 });
